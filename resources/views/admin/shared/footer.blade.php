6e90a769ad37ddf5c842353af16940d5cbafe93b
eller <ellermister@gmail.com>
2019-10-25 17:27:07 +0800

    初始化

---
+++ resources/views/admin/shared/footer.blade.php
@@ -0,0 +1,22 @@
+<!-- ============================================================== -->
+<!-- footer -->
+<!-- ============================================================== -->
+<div class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                Copyright © 2018 Concept. All rights reserved. Dashboard by <a href="https://colorlib.com/wp/">Colorlib</a>.
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                <div class="text-md-right footer-links d-none d-sm-block">
+                    <a href="javascript: void(0);">About</a>
+                    <a href="javascript: void(0);">Support</a>
+                    <a href="https://eller.tech">ELLER</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- end footer -->
+<!-- ============================================================== -->
